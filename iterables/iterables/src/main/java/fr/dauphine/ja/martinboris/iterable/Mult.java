099ed14f3b733ff1f2b23b78d63b308463516391
martbo140 <martbo140@b028-1.windows.dauphine.fr>
2019-11-25 15:08:33 +0100

    suite

--- iterables/iterables/src/main/java/fr/dauphine/ja/martinboris/iterable/Mult.java
+++ iterables/iterables/src/main/java/fr/dauphine/ja/martinboris/iterable/Mult.java
@@ -5,16 +5,19 @@ import java.util.AbstractSequentialList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
+
 //changer pour AbstractList
 public class Mult {
 	
-	public static List<Integer> mult(int x, List<Integer> l){
+	public static List<Integer> mult(final int x, final List<Integer> l){
 		//throws new UnsupportedOperationException();
 		return new AbstractSequentialList<Integer>() {
 			public ListIterator<Integer> listIterator(int index){
-				return new ListIterator<Integer>(index);
+				//return new ListIterator<Integer>(index);
+				return new ListIterator<Integer>();
 			}
-			private ListIterator<Integer> myLit = l.listIterator(index);
+			private ListIterator<Integer> myLit = l.listIterator();
+			//private ListIterator<Integer> myLit = l.listIterator(index);
 			//}
 			public boolean hasNext(){
 				return myLit.hasNext();
@@ -23,14 +26,13 @@ public class Mult {
 			public Integer next() {
 				return myLit.next()*x;
 			}
-			
+
+			public int size() {
+				return l.size();
+			}
 			//remove, set, add : throws new UnsupportedOperationException();
 		};
 
-	}
-	
-	public int size() {
-		return l.size();
 	}
 
 }
